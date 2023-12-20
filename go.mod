53439e1a35492fc71ee4463921f5ad12cbf934aa
Harish Vadaparty <harishvadaparty@gmail.com>
2023-12-20 13:58:04 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/harryalways317/gocraft
+
+go 1.21.5
---
+++ gocraft.go
@@ -0,0 +1,13 @@
+package gocraft
+
+const version = "1.0.0"
+
+type GoCraft struct {
+	AppName string
+	Debug   bool
+	Version string
+}
+
+func (g *GoCraft) New(rootPath string) error {
+
+}
